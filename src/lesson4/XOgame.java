3e24b4033f906009d8e04c9090ee8c9df9f430f5
maxim <estonmax@gmail.com>
2020-08-31 22:12:15 +0300

    1) Add logic of variant of victory for game 3x3 2) Add logic of variant of victory for game NxN 3) Add comp's logic

--- src/lesson4/XOgame.java
+++ src/lesson4/XOgame.java
@@ -4,8 +4,8 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class XOgame {
-    static final int SIZE = 3;
-//    static final int DOT_TO_WIN = 3;
+    static final int SIZE = 7;
+    static final int DOT_TO_WIN = 4;
 
     static final char DOT_X = 'X';
     static final char DOT_O = 'O';
@@ -23,7 +23,7 @@ public class XOgame {
         while (true) {
             humanTurn();
             printMap();
-            if(checkWin(DOT_X)){
+            if (checkWin(DOT_X)) {
                 System.out.println("Вы победили! Поздравляем!");
                 break;
             }
@@ -34,7 +34,7 @@ public class XOgame {
 
             aiTurn();
             printMap();
-            if(checkWin(DOT_O)){
+            if (checkWin(DOT_O)) {
                 System.out.println("Компьютер победил.");
                 break;
             }
@@ -88,23 +88,115 @@ public class XOgame {
             return false;
         }
         return map[y][x] == DOT_EMPTY;
-//        if (map[y][x] == DOT_EMPTY) {
-//            return true;
-//        } else {
-//            return false;
-//        }
     }
 
     public static void aiTurn() {
         int x;
         int y;
 
-        do {
-            x = random.nextInt(SIZE);
-            y = random.nextInt(SIZE);
-        } while (!isCellValid(y, x));
+        if (isChoicePlace()) {
+            return;
+        } else {
+            do {
+                x = random.nextInt(SIZE);
+                y = random.nextInt(SIZE);
+            }
+            while (!isCellValid(y, x));
+            map[y][x] = DOT_O;
+        }
+    }
 
-        map[y][x] = DOT_O;
+    /**
+     * 3. *** Доработать искусственный интеллект, чтобы он мог блокировать ходы игрока.
+     */
+
+    private static boolean isChoicePlace() { // ставит нолик, если находит (DOT_TO_WIN - 1) символов соперника и 1 пустую ячейку в ряду
+        int count = 0;
+        int countEmpty = 0;
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                // считаем количество символов соперника ВПРАВО ВВЕРХ и чтобы был 1 символ пустой
+                if (i + 1 - DOT_TO_WIN >= 0 && j + DOT_TO_WIN <= SIZE) {
+                    for (int k = 0; k < DOT_TO_WIN; k++) {
+                        if (map[i - k][j + k] == DOT_X) {
+                            count++;
+                        }
+                        if (map[i - k][j + k] == DOT_EMPTY) {
+                            countEmpty++;
+                            x = i - k;
+                            y = j + k;
+                        }
+                        if (count == DOT_TO_WIN - 1 && countEmpty == 1) {
+                            map[x][y] = DOT_O;
+                            return true;
+                        }
+                    }
+                    count = 0;
+                    countEmpty = 0;
+                }
+                // считаем количество символов соперника ВПРАВО и чтобы был 1 символ пустой
+                if (j + DOT_TO_WIN <= SIZE) {
+                    for (int k = 0; k < DOT_TO_WIN; k++) {
+                        if (map[i][j + k] == DOT_X) {
+                            count++;
+                        }
+                        if (map[i][j + k] == DOT_EMPTY) {
+                            countEmpty++;
+                            x = i;
+                            y = j + k;
+                        }
+                        if (count == DOT_TO_WIN - 1 && countEmpty == 1) {
+                            map[x][y] = DOT_O;
+                            return true;
+                        }
+                    }
+                    count = 0;
+                    countEmpty = 0;
+                }
+                // считаем количество символов соперника ВПРАВО ВНИЗ и чтобы был 1 символ пустой
+                if (i + DOT_TO_WIN <= SIZE && j + DOT_TO_WIN <= SIZE) {
+                    for (int k = 0; k < DOT_TO_WIN; k++) {
+                        if (map[i + k][j + k] == DOT_X) {
+                            count++;
+                        }
+                        if (map[i + k][j + k] == DOT_EMPTY) {
+                            countEmpty++;
+                            x = i + k;
+                            y = j + k;
+                        }
+                        if (count == DOT_TO_WIN - 1 && countEmpty == 1) {
+                            map[x][y] = DOT_O;
+                            return true;
+                        }
+                    }
+                    count = 0;
+                    countEmpty = 0;
+                }
+                // считаем количество символов соперника ВНИЗ и чтобы был 1 символ пустой
+                if (i + DOT_TO_WIN <= SIZE) {
+                    for (int k = 0; k < DOT_TO_WIN; k++) {
+                        if (map[i + k][j] == DOT_X) {
+                            count++;
+                        }
+                        if (map[i + k][j] == DOT_EMPTY) {
+                            countEmpty++;
+                            x = i + k;
+                            y = j;
+                        }
+                        if (count == DOT_TO_WIN - 1 && countEmpty == 1) {
+                            map[x][y] = DOT_O;
+                            return true;
+                        }
+                    }
+                    count = 0;
+                    countEmpty = 0;
+                }
+
+            }
+        }
+        return false;
     }
 
     public static boolean isFull() {
@@ -118,18 +210,102 @@ public class XOgame {
         return true;
     }
 
+    /**
+     * Ниже реализация задачи №1: Переделать проверку победы, чтобы она не была реализована просто набором условий,
+     * например, с использованием циклов.
+     * Будет скрыта, потому что есть Задача №2.
+     * @param c
+     * @return
+     */
+//    public static boolean checkWin(char c) {
+//        int countRoll = 0; //счетчик символов "char c" в строке
+//        int countColumn = 0; //счетчик символов "char c" в столбце
+//        int countDiagMain = 0; //счетчик символов "char c" в главной диагонали
+//        int countDiagSide = 0; //счетчик символов "char c" в побочной диагонали
+//        for (int i = 0; i < SIZE; i++) {
+//            for (int j = 0; j < SIZE; j++) {
+//                if (map[i][j] == c) {
+//                    countRoll++;
+//                }
+//                if (map[j][i] == c) {
+//                    countColumn++;
+//                }
+//                if (countColumn == SIZE || countRoll == SIZE) {
+//                    return true;
+//                }
+//            }
+//            countColumn = 0;
+//            countRoll = 0;
+//            if (map[i][i] == c) {
+//                countDiagMain++;
+//            }
+//            if (map[i][SIZE - 1 - i] == c) {
+//                countDiagSide++;
+//            }
+//            if (countDiagMain == SIZE || countDiagSide == SIZE) {
+//                return true;
+//            }
+//        }
+//
+//        return false;
+//    }
+
+    /**
+     * 2. * Попробовать переписать логику проверки победы, чтобы она работала для поля 5х5 и количества фишек 4
+     *
+     * @param c
+     * @return
+     */
     public static boolean checkWin(char c) {
-        if (map[0][0] == c && map[0][1] == c && map[0][2] == c) {return true; }
-        if (map[1][0] == c && map[1][1] == c && map[1][2] == c) {return true; }
-        if (map[2][0] == c && map[2][1] == c && map[2][2] == c) {return true; }
-
-        if (map[0][0] == c && map[1][0] == c && map[2][0] == c) {return true; }
-        if (map[0][1] == c && map[1][1] == c && map[2][1] == c) {return true; }
-        if (map[0][2] == c && map[1][2] == c && map[2][2] == c) {return true; }
-
-        if (map[0][0] == c && map[1][1] == c && map[2][2] == c) {return true; }
-        if (map[0][2] == c && map[1][1] == c && map[2][0] == c) {return true; }
-
+        int count = 0; // счетчик количества символов
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (i + 1 - DOT_TO_WIN >= 0 && j + DOT_TO_WIN <= SIZE) { // считаем количество символов вправо вверх
+                    for (int k = 0; k < DOT_TO_WIN; k++) {
+                        if (map[i - k][j + k] == c) {
+                            count++;
+                        }
+                        if (count == DOT_TO_WIN) {
+                            return true;
+                        }
+                    }
+                    count = 0;
+                }
+                if (j + DOT_TO_WIN <= SIZE) { // считаем количество символов вправо
+                    for (int k = 0; k < DOT_TO_WIN; k++) {
+                        if (map[i][j + k] == c) {
+                            count++;
+                        }
+                        if (count == DOT_TO_WIN) {
+                            return true;
+                        }
+                    }
+                    count = 0;
+                }
+                if (i + DOT_TO_WIN <= SIZE && j + DOT_TO_WIN <= SIZE) { // считаем количество символов вправо вниз
+                    for (int k = 0; k < DOT_TO_WIN; k++) {
+                        if (map[i + k][j + k] == c) {
+                            count++;
+                        }
+                        if (count == DOT_TO_WIN) {
+                            return true;
+                        }
+                    }
+                    count = 0;
+                }
+                if (i + DOT_TO_WIN <= SIZE) {
+                    for (int k = 0; k < DOT_TO_WIN; k++) { // считаем количество символов вниз
+                        if (map[i + k][j] == c) {
+                            count++;
+                        }
+                        if (count == DOT_TO_WIN) {
+                            return true;
+                        }
+                    }
+                    count = 0;
+                }
+            }
+        }
         return false;
     }
 
